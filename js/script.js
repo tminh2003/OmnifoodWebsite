552f9a51edc1b9f094346241d85cdad53d339d7b
To, Minh <60716809+tminh2003@users.noreply.github.com>
2025-02-06 11:30:21 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const closeButton = document.getElementsByClassName(
+  "nav-mobile-button-close"
+)[0];
+const listButton = document.getElementsByClassName("nav-mobile-button-list")[0];
+
+closeButton.addEventListener("click", function (event) {});
+
+listButton.addEventListener("click", function (event) {
+  console.log("list");
+});
