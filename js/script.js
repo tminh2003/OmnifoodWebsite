b954a09e53a57fba5e6bcada6d887d9777b8a6fb
To, Minh <60716809+tminh2003@users.noreply.github.com>
2025-02-06 18:23:14 -0600

    implemented smooth scrolling
    added events to links to smooth scroll

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 const mobileNavClose = document.querySelector(".nav-mobile-button-close");
 const listButton = document.querySelector(".nav-mobile-button-list");
 const mobileNavHeader = document.querySelector(".header");
+const allLinks = document.querySelectorAll("a:link");
+const allNavLinks = document.querySelectorAll(".main-nav a");
+
+console.log(allNavLinks);
 
 mobileNavClose.addEventListener("click", function () {
   mobileNavHeader.classList.toggle("nav-open");
 });
 listButton.addEventListener("click", function (event) {
   mobileNavHeader.classList.toggle("nav-open");
+});
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    } else {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+allNavLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    mobileNavHeader.classList.toggle("nav-open");
+  });
 });
